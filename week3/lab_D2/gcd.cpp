5bd8a15fe55e3b51b5437b880597af415ba7e249
uday <24b2729@iitb.ac.in>
2025-01-23 12:49:02 +0000

    don ethe gcd but have scope in improving the time use

--- week3/lab_D2/gcd.cpp
+++ week3/lab_D2/gcd.cpp
@@ -5,10 +5,15 @@ main_program{
  int num2;
  cin >> num1;
  cin >> num2;
- cout << num1 << num2 ;
 int result = 1;
 
  int i = 2;
+if (num1 == 0){
+    result =num2;
+}
+if(num2 == 0){
+    result = num1;
+}
 
  
  while (i <= num1 && i<=  num2){
@@ -17,18 +22,13 @@ int result = 1;
     num1 = num1 /i;
     num2 = num2 /i;
     result  *= i;
-    std::cout << "result everytime " << result << "num1: " <<num1 << "num2: "<<num2 << std::endl;
   }
   i++;
-  std::cout << "If num and num2 is divisible by i " << i <<std::endl;
-  std::cout << "Final result after every unique " << result << std::endl;
 }
 else{
     i++;
-    std::cout << "If num1 and num2 is not divisible by i " << i<< std::endl;
     
 }
-std::cout <<"attempting to find final result" << result <<std::endl;
-
 }
+std::cout << result <<std::endl;
 }
