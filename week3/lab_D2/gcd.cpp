c9b4ed1f3dcc940280d0a3866e8472cc6634ed80
uday <24b2729@iitb.ac.in>
2025-01-23 12:37:12 +0000

    improved version better scope of improvement

--- week3/lab_D2/gcd.cpp
+++ week3/lab_D2/gcd.cpp
@@ -1,29 +1,34 @@
 #include <simplecpp>
 
 main_program{
-    int num1;
-    int num2; // when i am keeping the float why it is throing the eroor
-    cin >> num1 >> num2;
-    int i = 1;
-    int result = 1;
-        if (num1 ==0){
-            result = num2;
-        }
-        if(num2 == 0 ){
-            result =   num1;
-        }
-    while( i<= num1 && i<=num2){
-        if ((num1 % i==0) && (num2 % i==0)){
-            while ((num1 % i==0) && (num2 % i==0))
-            {
-            num1 = num1 /i ;
-            num2 = num2 /i;
-            
-            result *= i;
-            }
-        }
-        i++;
+ int num1;
+ int num2;
+ cin >> num1;
+ cin >> num2;
+ cout << num1 << num2 ;
+int result = 1;
 
-    }
-    std::cout << result <<std::endl;
+ int i = 2;
+
+ 
+ while (i <= num1 && i<=  num2){
+ if ((num1 %i ==0)&&(num2 %i ==0)){
+  while (num1 %i ==0 && num2 %i ==0){
+    num1 = num1 /i;
+    num2 = num2 /i;
+    result  *= i;
+    std::cout << "result everytime " << result << "num1: " <<num1 << "num2: "<<num2 << std::endl;
+  }
+  i++;
+  std::cout << "If num and num2 is divisible by i " << i <<std::endl;
+  std::cout << "Final result after every unique " << result << std::endl;
+}
+else{
+    i++;
+    std::cout << "If num1 and num2 is not divisible by i " << i<< std::endl;
+    
+}
+std::cout <<"attempting to find final result" << result <<std::endl;
+
+}
 }
