939703cd256e575c2a3982b8da0bd178d2ca01ed
uday <24b2729@iitb.ac.in>
2025-01-27 15:57:29 +0000

    some ways to make a spiral and try to do it better

---
+++ extra/spiral.cpp
@@ -0,0 +1,17 @@
+#include <simplecpp>
+main_program{
+int length = 5;
+turtleSim();
+repeat(100){
+    left(36);
+    forward(length);
+    right(144);
+    length = length +10;
+    forward(length);
+    right(36);
+    length = length +10;
+
+
+}
+getClick();
+}
