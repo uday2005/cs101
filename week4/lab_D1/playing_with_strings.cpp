867f8fad996047712f2cdbebde4569e383b9217a
uday <24b2729@iitb.ac.in>
2025-01-30 21:44:43 +0000

    done the playing_with_strings complete

--- week4/lab_D1/playing_with_strings.cpp
+++ week4/lab_D1/playing_with_strings.cpp
@@ -1,39 +1,43 @@
 #include <simplecpp>
 #include <string>
-main_program{
-    int length_1 ;
-    string word_1 ;
-    std::cin >> word_1;
-    std::cin >> length_1;
-    int weight = 0;
-    for (int i=0;i<length_1;i++){
-        cout << endl << i << endl;
-       char yes  = word_1[i]; 
-       cout  << yes;
+
+int result (int length,string word){
+int weight = 0;
+    for (int i=0;i<length;i++){
+       char yes  = word[i]; 
        
         if (yes == 'o' || yes == 'a' ||yes == 'i' || yes=='e' || yes=='u'){
             weight = weight +  3+ 2;
-            std::cout << "small-vowel-weight: " << weight << yes;
         }
         else if (yes == 'A'||yes =='E' || yes =='I' ||yes =='O'||yes == 'U'){
             weight = weight + 3 -1;
-            std::cout << "Capital-Vowel-Weight: " << weight  << endl;
         }
         else if ((int(yes) >= 65 && int(yes) <= 90)){
             weight  = weight + int(yes)-65+1-1;
-            std::cout << "Capital-Consonant-Weight: " << weight << endl;
         }
         else if (int(yes)>=97 && int(yes)<=122){
-            cout << yes << endl;
-            cout << int(yes);
             weight = weight + int(yes)- 97 +1+2;
-        std::cout << "small-consonant-Weight: " << weight << endl;
         }
         else{
             int value = int(yes);
             weight = weight+ (value %10);
-            std::cout << "special-char-weight: " << weight << endl;
         }
     }
-    std::cout << weight;
+      return weight;
+}
+main_program{
+    int length_1 ,length_2;
+    string word_1 ,word_2 ;
+    std::cin >> length_1 >> length_2 ;
+    std::cin >> word_1;
+    std::cin >> word_2;
+
+    int result_1 = result(length_1,word_1);
+    int result_2 = result(length_2,word_2);
+
+    int differnce = abs(result_1-result_2);
+    std::cout << differnce << " ";
+    if (differnce <10) std::cout  << "Close";
+    else if(differnce >=10 && differnce <50) std::cout << "Moderate";
+    else std::cout << "Far";
     }
