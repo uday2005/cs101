fa8640716232dd5f9be069dc13140017a2beed4b
uday <24b2729@iitb.ac.in>
2025-02-09 16:19:53 +0000

    done

---
+++ week5/lab_D2/valid_2048_tile.cpp
@@ -0,0 +1,28 @@
+#include <simplecpp>
+bool is_Valid (unsigned int num){
+
+if (num==0) return false;
+  int count = 0;
+    while (num >0){
+        if(num & 1){
+            count ++;
+        }
+        num = num >> 1;
+
+    }
+    return count==1;
+}
+main_program{
+
+int test_cases;
+cin >> test_cases;
+
+for (int i=0;i<test_cases;i++){
+    long unsigned int n;
+    cin >> n;
+    bool g = is_Valid(n);
+
+    cout << (g ? "yes" : "no")<< endl;;
+}
+
+}
