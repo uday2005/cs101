30a0e985821c2cca4b4d11ec3a700cefa64a286e
uday <24b2729@iitb.ac.in>
2025-02-16 11:45:16 +0000

    week4

--- week4/lab_D1/square_root.cpp
+++ week4/lab_D1/square_root.cpp
@@ -23,3 +23,36 @@ main_program{
 
     std::cout  <<std::fixed<< std::setprecision(6) << (high + low)/2;
 }
+
+double  newtonsqrt(double n){
+    const float ephsilon = 0.00001;
+    double new_x;
+    double x = n; // initial guess
+    while (true){
+        new_x = 0.5 *(x+n/x);
+
+        if (abs(x-new_x) < ephsilon){
+            break;
+        }
+        x= new_x;
+    }
+    return x;
+}
+
+double binarysqrt(int n){
+    double ephsilon = 0.000001;
+    double high = n;
+    double low = 0;
+
+    if (n <1) high = 1;  // Fix the number b/w 0 an d 1.
+
+    while ((high - low) >= ephsilon){
+        double mid = (high + low) /2;
+
+        if (mid * mid > n) high = mid;
+        else low = mid;
+    }
+    return (high + low)/2;
+}
+
+// we can also make it more efficient by making it working for less than 1 
--- week4/lab_D2/string_weight.cpp
+++ week4/lab_D2/string_weight.cpp
@@ -1,7 +1,113 @@
-// so the first problem incurring is i don't know how to claculate the length of string.
 
-#include <simplecpp>
-main_program{
-    
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    cout << int('a') << int('A') ;
+    int n,m;
+    cin >> n>>m;
+
+    int result1 = 0;
+    int result2 = 0;
+        int num_vowels = 0;
+        int num_conso = 0;
+    for (int i = 0;i<n;i++){
+        char s;
+        cin >> s;
+        if(s=='a' || s == 'e' || s == 'i' || s == 'o' || s == 'u'){
+            num_vowels ++;
+
+            if(num_conso != 0){
+                int w = 1;
+                while(num_conso >0){
+                    w *= 2;
+                    num_conso--;
+                }
+                result1 += w;
+            }
+        }
+
+        else{
+            num_conso++;
+
+            if(num_vowels != 0){
+                int w = 1;
+                while(num_vowels >0){
+                    w *= 3;
+                    num_vowels--;
+                }
+                result1 += w;
+            }
+
+        }
+    }
+                if(num_conso != 0){
+                int w = 1;
+                while(num_conso >0){
+                    w *= 2;
+                    num_conso--;
+                }
+                result1 += w;
+            }
+                if(num_vowels != 0){
+                int w = 1;
+                while(num_vowels >0){
+                    w *= 3;
+                    num_vowels--;
+                }
+                result1 += w;
+            }
+
+        for (int i = 0;i<m;i++){
+        char s;
+        cin >> s;
+        if(s=='a' || s == 'e' || s == 'i' || s == 'o' || s == 'u'){
+            num_vowels ++;
+
+            if(num_conso != 0){
+                int w = 1;
+                while(num_conso >0){
+                    w *= 2;
+                    num_conso--;
+                }
+                result2 += w;
+            }
+        }
+
+        else{
+            num_conso++;
+
+            if(num_vowels != 0){
+                int w = 1;
+                while(num_vowels >0){
+                    w *= 3;
+                    num_vowels--;
+                }
+                result2 += w;
+            }
+
+        }
+    }
+                if(num_conso != 0){
+                int w = 1;
+                while(num_conso >0){
+                    w *= 2;
+                    num_conso--;
+                }
+                result2 += w;
+            }
+                if(num_vowels != 0){
+                int w = 1;
+                while(num_vowels >0){
+                    w *= 3;
+                    num_vowels--;
+                }
+                result2 += w;
+            }
+
+cout << result1 <<  " " << result2 << " " << result1 - result2;
+
 }
- 
+
